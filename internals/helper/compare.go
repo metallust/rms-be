54b999aadb97f1ab595f6ddd8ec1d7322eeaf321
metallustWSL <shaikh.irfan.oct16@gmail.com>
2024-08-04 20:13:38 +0530

    add required field in sample.env

---
+++ internals/helper/compare.go
@@ -0,0 +1,9 @@
+package helper
+
+import "github.com/metallust/rms-be/internals/models"
+
+func Compare(profile models.Profile, job models.Job) int {
+    // Compare the profile and job
+    // Return a score of how well the profile matches the job
+    return 0
+}
